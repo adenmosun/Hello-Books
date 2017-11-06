c42b3843ccbdaf208bf5b160a8705653ae31d40d
adenmosun <tayo.adenmosun@gmail.com>
2017-11-06 06:20:21 +0100

    commiy other initial changes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,34 @@
 const express = require('express');
 const bodyParser = require ('body-parser')
 const app = express ();
-const todosController = require('../controllers').todos;
+const booksController = require('../controllers').books;
 
 module.exports = (app) => {
   app.get('/api', (req, res) => res.status(200).send({
     message: 'Welcome to the Todos API!',
   }));
 
-  app.post('/api/books', todosController.create);
+  app.post('/api/books', booksController.create);
+
+  app.get('/api/books', booksController.list);
+
+  // app.put( '/api/books/:id', booksController.modify);
+
+  // app.post('/api/users/:userid/borrow/:bookid', booksController.borrow);
+
+  // app.post('/api/users/:userid/return/:bookid', booksController.return);
+
+  // app.put('/api/users/:userid/borrow/:bookid', booksController.borrow-request);
+
+  // app.put('/api/users/:userid/return/:bookid', booksController.return-request);
+
+  // app.post('/api/users/:userid/review/:bookid', booksController.review);
+
+  // app.post('/api/users/:userid/fav/:bookid', booksController.favnum);
+
+  // app.get('/api/users/:userid/favbooks', booksController.favbooks);
+
+  // app.get('/api/books?sort=upvotes&order=desc', booksController.vote);
+
+
 };
